d4d106dc7a083558d893b2954933dced42d3e98d
Nirmalasainee <sainee.n158@gmail.com>
2025-01-09 23:47:26 +0530

    recursion with parameter

--- recursion/functional_recursion.rb
+++ recursion/functional_recursion.rb
@@ -7,3 +7,14 @@ def print_sum(i)
 end 
 n = gets.chomp.to_i
 puts print_sum(n)
+
+# Print factorial on N using recursion
+ 
+def fact(i)
+    if (i == 0)
+      return 1
+    end
+    return i * fact(i-1)
+end 
+n = gets.chomp.to_i
+puts fact(n)
